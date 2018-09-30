320bb007d6fe02f86853dfc4e5bd468b460a1234
manjit <manjitp46@gmail.com>
2018-09-30 15:07:14 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var app = express();
+
+var childProcess = require('child_process');
+
+var PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    var command = "ifconfig | grep \"inet addr\" | sed 's/ *inet addr://'"
+    childProcess.exec(command,(err,stdout,stderr)=>{
+        if(err) {
+            res.status(500).send({
+                Error: true,
+                Message: `Failed to execute command: ${command}`
+            })
+        }else  if(stderr) {
+            res.status(500).send({
+                Error: true,
+                Message: `Failed to execute command: ${command} :Stderr occured`
+            })
+        }else {
+            res.status(200).send({
+                Error: false,
+                Message: `{"Your ip adress are",${stdout.toString()}}`
+            }) 
+        }
+    })
+})
+
+app.listen(3000,()=>{
+    console.log(`Node app stared at port@ ${PORT} with process id=${process.pid}`)
+})
